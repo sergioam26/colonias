94910898c81d8354f209a0108dc2fc7104697866
sergioam26 <sergioalgmir@gmail.com>
2026-02-17 09:06:38 +0100

    Prueba

--- backend/src/test/java/com/example/demo/BackendApplicationTests.java
+++ backend/src/test/java/com/example/demo/BackendApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import com.colonias.backend.BackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (classes = BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
